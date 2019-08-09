840593b3bc05d37899118203e84c6a5fb0a4fdd5
Yevgeni Gurevitch <evgeni.gurvitch@ironsrc.com>
2019-08-09 09:51:01 +0300

    accept positive ints only

--- args.go
+++ args.go
@@ -11,12 +11,12 @@ func readArgs() (int64, uint, error) {
 		return 0, 0, fmt.Errorf("usage: %s [ttl] [threshold]\n", os.Args[0])
 	}
 	ttl, err := strconv.ParseInt(os.Args[1], 10, 64)
-	if err != nil {
-		return 0, 0, fmt.Errorf("ttl value must be an integer, got %s\n", os.Args[1])
+	if err != nil || ttl < 0 {
+		return 0, 0, fmt.Errorf("ttl value must be a positive integer, got %s\n", os.Args[1])
 	}
 	threshold, err := strconv.ParseInt(os.Args[2], 10, 64)
-	if err != nil {
-		return 0, 0, fmt.Errorf("threshold value must be an integer, got %s\n", os.Args[2])
+	if err != nil || threshold < 0 {
+		return 0, 0, fmt.Errorf("threshold value must be a positive integer, got %s\n", os.Args[2])
 	}
 	return ttl, uint(threshold), nil
 }
